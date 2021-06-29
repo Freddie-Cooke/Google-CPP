ecf90e8d401a1fc56b90737ffd4b9d7ec82d9071
Freddie-Cooke <Freddie.mac@hotmail.co.uk>
2021-06-29 11:06:31 +0100

    Create books.cpp

---
+++ Next Steps/books.cpp
@@ -0,0 +1,45 @@
+// books.cpp
+
+#include <iostream>
+#include <string>
+
+// books are required or optional, required are more expensive
+// new and used books are also priced differently
+// 
+
+// take input of books
+// user provides the code
+// the cost
+// the number of books avaliable
+// new/used requried/optional
+
+class Book {
+
+public:
+	//
+
+private:
+	int price_;
+	int num_of_copies_;
+	string code_;
+	string condition_;
+	string requirement_;
+}
+
+void Welcome_message() {
+
+}
+
+void Book_input() {
+
+}
+
+// show the total cost of the books
+// show the profit 20% of the list price
+
+int main() {
+
+
+
+	return 0;
+}
